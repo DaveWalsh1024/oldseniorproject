a78dddc139dfacfebc689f86f930d03a07aac466
Jack Lavallee <myemailisjack@gmail.com>
2017-11-28 15:24:08 -0500

    Branch Check

---
+++ app/src/main/java/com/example/david/scorecardpro/ScoringActivity.java
@@ -0,0 +1,19 @@
+package com.example.david.scorecardpro;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+/**
+ * Created by jacklavallee on 11/28/17.
+ */
+
+public class ScoringActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scoring);
+    }
+}
