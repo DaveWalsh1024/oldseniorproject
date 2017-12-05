1bf54d8b8b7883a793c25eae5552312d9482f390
Jack Lavallee <myemailisjack@gmail.com>
2017-12-05 15:01:26 -0500

    Test

--- app/src/main/java/com/example/david/scorecardpro/LoginActivity.java
+++ app/src/main/java/com/example/david/scorecardpro/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.david.scorecardpro;
 
 import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,8 +15,29 @@ public class LoginActivity extends AppCompatActivity  {
         setContentView(R.layout.activity_login);
     }
 
-    public void switchPage(View view) {
-        Intent intent = new Intent(this, ScoringActivity.class);
-        startActivity(intent);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        Intent go;
+        switch(item.getItemId())
+        {
+            case R.id.activity_login:
+                go = new Intent(this,LoginActivity.class);
+                startActivity(go);
+                return true;
+
+            case R.id.activity_scoring:
+                go = new Intent(this,ScoringActivity.class);
+                startActivity(go);
+                return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
